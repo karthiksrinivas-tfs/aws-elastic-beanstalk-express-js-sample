8ab5354e1951174a591dea4af3e648219c309413
Karthik Srinivas <karthik.srinivas@thermofisher.com>
2021-03-17 04:02:29 -0400

    Full pipeline test after adding Manual Review Stage

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Testing CI/CD pipeline'));
+app.get('/', (req, res) => res.send('Testing CI/CD pipeline inside AWS'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
