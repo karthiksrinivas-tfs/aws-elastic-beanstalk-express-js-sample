a006cc131f9ad1d77aa5cdb2a3f33e2d9ca6ce98
Karthik Srinivas <karthik.srinivas@thermofisher.com>
2021-03-17 03:57:37 -0400

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello Dear World!'));
+app.get('/', (req, res) => res.send('Testing CI/CD pipeline'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
